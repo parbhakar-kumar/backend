1b58bf29f8556ee34cb184e8202a0c994c1c1cf9
prabhakar <prabhakarpandey7061@gmail.com>
2025-04-18 11:31:57 +0530

    added stlying

--- farntend/src/App.jsx
+++ farntend/src/App.jsx
@@ -1,15 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+import React from 'react'
+
 import './App.css'
 
+
 function App() {
-  const [jokes, setJokes]=useState([])
+  const [jokes, setJokes] = useState([])
+
+
+  useEffect(()=>{
+    axios.get(' htttp://localhost:3000/jokes ')
+    .then((response)=>{
+      setJokes(response.data)
+    })
+    .catch((error)=>{
+      console.log(error)
+    })
+  })
 
   return (
     <>
- <h1>full stack </h1>
- <P>jokes:{jokes.length}</P>
+      <h1>full stack </h1>
+      <p>jokes: {jokes.length}</p>
+      {
+        jokes.map((joke, index) => (
+          <div key={joke.id}>
+            <h3>{joke.title}</h3>
+            <p>{joke.content}</p>
+          </div>
+        ))
+      }
     </>
   )
 }
